2803b09c04a8aa7e35a703eb7934de3708e381db
maxwel84 <maxweloliveira@hotmail.com>
2025-01-26 15:40:42 +0000

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,30 @@
-/*
-OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartao da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-     - passo 3 - fazer aparecer o próximo cartão da lista 
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-
-OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-  - passo 3 - fazer aparecer o cartão anterior da lista
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-*/
-//OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartao da lista
-
-// passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
-// passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+
 btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
 
-    if (cartaoAtual === cartoes.length - 1) return;
-
-
-    //- passo 4 - buscar o cartão que esta selecionado e esconder
-    escondeCartaoSelecionado();
-
-    //- passo 3 - fazer aparecer o próximo cartão da lista 
-    cartaoAtual++;
-    cartoes[cartaoAtual].classList.add("selecionado");
-
+  esconderCartaoSelecionado();
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
 });
-
-/* OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-  - passo 3 - fazer aparecer o cartão anterior da lista
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-
-*/
-
 
 btnVoltar.addEventListener("click", function () {
-
   if (cartaoAtual === 0) return;
 
-  const cartaoSelecionado = document.querySelector(".selecionado");
-  cartaoSelecionado.classList.remove("selecionado");
+  esconderCartaoSelecionado();
 
   cartaoAtual--;
-  cartoes[cartaoAtual].classList.add("selecionado");
-
+  mostrarCartao(cartaoAtual);
 });
 
-function escondeCartaoSelecionado() {
+function mostrarCartao(cartaoAtual) {
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
   const cartaoSelecionado = document.querySelector(".selecionado");
   cartaoSelecionado.classList.remove("selecionado");
 }
